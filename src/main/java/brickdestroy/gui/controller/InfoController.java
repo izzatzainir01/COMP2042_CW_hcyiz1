c4ffe7d6010341f5bc28bedd4d0ace878ce394d2
Izzat Z <hcyiz1@nottingham.edu.my>
2021-11-30 11:42:39 +0800

    Modified the other Controllers to extend AbstractController

--- src/main/java/brickdestroy/gui/controller/InfoController.java
+++ src/main/java/brickdestroy/gui/controller/InfoController.java
@@ -1,21 +1,13 @@
 package brickdestroy.gui.controller;
 
-import java.awt.Component;
-import java.awt.Dimension;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 
-import javax.swing.JPanel;
-
 import brickdestroy.gui.MainFrame;
 import brickdestroy.gui.view.MenuInfoView;
 
-public class InfoController extends JPanel {
-
-    private MainFrame frame;
-    private int width = MainFrame.getWidth();
-    private int height = MainFrame.getHeight();
+public class InfoController extends AbstractController {
 
     private MenuInfoView description;
     private MenuInfoView controls;
@@ -30,42 +22,12 @@ public class InfoController extends JPanel {
      * @param frame The {@code MainFrame}
      */
     public InfoController(MainFrame frame) {
-
-        // Define frame
-        this.frame = frame;
-
-        // Initialise the panel's properties
-        this.setBounds(0, 0, width, height);
-        this.setPreferredSize(new Dimension(width, height));
-        this.setLayout(null);
+        // Call the super constructor
+        super(frame);
 
         // Add the description view
         addView(description = new MenuInfoView("Description", getContent("description.txt"), "Controls"));
         initDescriptionButtonsListeners();
-    }
-
-    /**
-     * Add a {@code Component} to this controller and automatically call
-     * {@code revalidate()} and {@code repaint()}.
-     * 
-     * @param comp The component to be added
-     */
-    private void addView(Component comp) {
-        this.add(comp);
-        revalidate();
-        repaint();
-    }
-
-    /**
-     * Remove a {@code Component} from this controller and automatically call
-     * {@code revalidate()} and {@code repaint()}.
-     * 
-     * @param comp The component to be removed
-     */
-    private void removeView(Component comp) {
-        this.remove(comp);
-        revalidate();
-        repaint();
     }
 
     /**
--- src/main/java/brickdestroy/gui/controller/MenuController.java
+++ src/main/java/brickdestroy/gui/controller/MenuController.java
@@ -1,55 +1,29 @@
 package brickdestroy.gui.controller;
 
-import javax.swing.JPanel;
-
 import brickdestroy.gui.MainFrame;
 import brickdestroy.gui.view.MenuHomeView;
 
-import java.awt.Component;
-import java.awt.Dimension;
-
-public class MenuController extends JPanel {
-
-    private MainFrame frame;
-    private int width = MainFrame.getWidth();
-    private int height = MainFrame.getHeight();
+public class MenuController extends AbstractController {
 
     private MenuHomeView home;
 
     /**
      * The {@code MenuController} class is the Controller for the game's Main Menu,
-     * which includes the {@code MenuHome} and {@code MenuInfo} views. It is
-     * responsible for getting the user input to switch between the two views.
+     * which includes the {@code MenuHome} view. It is responsible for getting the
+     * user input to switch to the {@code InfoController}.
      * <p>
      * The {@code GameFrame} parameter is necessary in order for the MenuController
      * to call the GameFrame to change Controllers.
      * 
-     * @param frame - The {@code GameFrame}.
+     * @param frame - The {@code MainFrame}.
      */
     public MenuController(MainFrame frame) {
-        // Define frame
-        this.frame = frame;
-
-        // Initialise the panel's properties
-        this.setBounds(0, 0, width, height);
-        this.setPreferredSize(new Dimension(width, height));
-        this.setLayout(null);
+        // Call the super constructor
+        super(frame);
 
         // Add the home view upon first launch
         addView(home = new MenuHomeView());
         initHomeButtonsListeners();
-    }
-
-    /**
-     * Add a {@code Component} to this controller and automatically call
-     * {@code revalidate()} and {@code repaint()}.
-     * 
-     * @param comp The component to be added
-     */
-    private void addView(Component comp) {
-        this.add(comp);
-        revalidate();
-        repaint();
     }
 
     /**
