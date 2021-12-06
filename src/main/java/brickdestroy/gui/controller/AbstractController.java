4eeb491af8910ffbf24663ec93c42086511aa1c2
Izzat Z <hcyiz1@nottingham.edu.my>
2021-12-07 02:32:42 +0800

    Improved javadocs on the AbstractController

--- src/main/java/brickdestroy/gui/controller/AbstractController.java
+++ src/main/java/brickdestroy/gui/controller/AbstractController.java
@@ -9,6 +9,26 @@ import javax.swing.JPanel;
 import brickdestroy.gui.MainFrame;
 import brickdestroy.gui.view.MyAbstractView;
 
+/**
+ * An abstract class that represents a Controller for this game. It provides a
+ * template that all Controllers in this game use to ease the process of
+ * creating a new Controller. The Controllers in this game uses a {@code JPanel}
+ * as an in-between for the {@code MainFrame} and the Views that it adds itself.
+ * <p>
+ * I am aware that adding Views to the {@code JPanel} makes it more like the
+ * 'display' than the {@code MainFrame}, who is supposed to have that role.
+ * However, I simply couldn't figure out how to add and remove Views from the
+ * frame without it being very buggy. So I settled with thinking of the
+ * {@code JPanel} as more of a screen while the {@code MainFrame} as more of a
+ * television box that holds the screen.
+ * <p>
+ * The {@code AbstractController} is responsible for predefining some properties
+ * of the {@code JPanel} that it uses and methods to add and remove a View from
+ * the JPanel.
+ * 
+ * @see MainFrame
+ * @see JPanel
+ */
 public abstract class AbstractController implements ActionListener {
 
     protected MainFrame frame;
@@ -18,14 +38,26 @@ public abstract class AbstractController implements ActionListener {
     protected JPanel panel;
 
     /**
-     * The {@code AbstractController} class is an abstract class that represents a
-     * Controller for this game. It provides a template that all Controllers in this
-     * game can use in order to avoid reusing the same code.
+     * An abstract class that represents a Controller for this game. It provides a
+     * template that all Controllers in this game use to ease the process of
+     * creating a new Controller. The Controllers in this game uses a {@code JPanel}
+     * as an in-between for the {@code MainFrame} and the Views that it adds itself.
      * <p>
-     * It is responsible for predefining some properties of the {@code JPanel} that
-     * it extends and a method to add and remove a View from itself.
+     * I am aware that adding Views to the {@code JPanel} makes it more like the
+     * 'display' than the {@code MainFrame}, who is supposed to have that role.
+     * However, I simply couldn't figure out how to add and remove Views from the
+     * frame without it being very buggy. So I settled with thinking of the
+     * {@code JPanel} as more of a screen while the {@code MainFrame} as more of a
+     * television box that holds the screen.
+     * <p>
+     * The {@code AbstractController} is responsible for predefining some properties
+     * of the {@code JPanel} that it uses and methods to add and remove a View from
+     * the {@code JPanel}.
      * 
      * @param frame The {@code MainFrame}
+     * 
+     * @see MainFrame
+     * @see JPanel
      */
     protected AbstractController(MainFrame frame) {
 
@@ -42,19 +74,19 @@ public abstract class AbstractController implements ActionListener {
     }
 
     /**
-     * Get the {@code JPanel} of this controller. This is only meant for the
-     * {@code MainFrame}
-     * to use but I couldn't figure out a more 'secure' way of doing this lmao.
+     * Gets the {@code JPanel} of this controller. This is only meant for the
+     * {@code MainFrame} to use but I couldn't figure out a more 'secure' way of
+     * doing this.
      * 
-     * @return A {@code JPanel} of this controller
+     * @return The {@code JPanel} of this controller
      */
     public JPanel getPanel() {
         return panel;
     }
 
     /**
-     * Add a {@code JLabel} to this controller. This method automatically calls the
-     * {@code revalidate()} and {@code repaint()} methods upon adding a View.
+     * Adds a {@code JLabel} to this controller. This method automatically calls the
+     * {@code revalidate} and {@code repaint} methods upon adding a View.
      * 
      * @param view The new view to be added to this controller
      */
@@ -65,8 +97,8 @@ public abstract class AbstractController implements ActionListener {
     }
 
     /**
-     * Add a {@code MyAbstractView} to this controller. This method automatically
-     * calls the {@code revalidate()} and {@code repaint()} methods upon adding a
+     * Adds a {@code MyAbstractView} to this controller. This method automatically
+     * calls the {@code revalidate} and {@code repaint} methods upon adding a
      * View. It also automatically adds this controller as an {@code ActionListener}
      * to the given View.
      * 
@@ -80,11 +112,10 @@ public abstract class AbstractController implements ActionListener {
     }
 
     /**
-     * Remove a {@code JLabel}, or a View, from this controller. This method
-     * automatically calls the {@code revalidate()} and {@code repaint()} methods
-     * upon removing a View.
+     * Removes a View from this controller. This method automatically calls the
+     * {@code revalidate} and {@code repaint} methods upon removing the View.
      * 
-     * @param view The new view to be removed from this controller
+     * @param view The view to be removed from this controller
      */
     protected void removeView(JLabel view) {
         panel.remove(view);
