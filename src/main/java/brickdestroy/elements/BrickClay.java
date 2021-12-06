74057a80c031f24eb6f0a44a45edcead12a527d5
Izzat Z <hcyiz1@nottingham.edu.my>
2021-12-07 00:44:51 +0800

    Improved javadoc comments in Bricks classes and minor refactoring

--- src/main/java/brickdestroy/elements/BrickClay.java
+++ src/main/java/brickdestroy/elements/BrickClay.java
@@ -7,6 +7,12 @@ import java.awt.Rectangle;
 import java.awt.Shape;
 import java.awt.geom.Point2D;
 
+/**
+ * A child class of {@link Brick}. It is the lowest level brick of the game and
+ * has the lowest strength. Its {@link Shape} is that of a {@link Rectangle}.
+ * <p>
+ * It is responsible for defining its colour and strength.
+ */
 public class BrickClay extends Brick {
 
     private static final Color BORDER = Color.GRAY;
@@ -14,14 +20,15 @@ public class BrickClay extends Brick {
     private static final int STRENGTH = 1;
 
     /**
-     * The {@code BrickClay} class is a child class of {@code Brick}. It has a
-     * {@code strength} of 1.
+     * A child class of {@link Brick}. It is the lowest level brick of the game and
+     * has the lowest strength. It is a rectangular shaped brick with a gray border
+     * and a dark red inner colour that is immediately broken upon impact.
      * <p>
-     * It is only responsible for defining its colours.
+     * It is responsible for defining its colour and its strength.
      * 
-     * @param pos    - The top left corner.
-     * @param width  - The width.
-     * @param height - The height.
+     * @param pos    The top left corner
+     * @param width  The width
+     * @param height The height
      */
     public BrickClay(Point2D pos, int width, int height) {
         super(pos, width, height, STRENGTH, BORDER, INNER);
@@ -29,18 +36,13 @@ public class BrickClay extends Brick {
 
     /**
      * {@inheritDoc}
+     * <p>
+     * The {@code BrickClay} class uses a {@link Rectangle} to define its
+     * {@code Shape}.
      */
     @Override
     protected Shape makeBrickFace(Point2D pos, int width, int height) {
         return new Rectangle((Point) pos, new Dimension(width, height));
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    protected Shape getBrick() {
-        return super.getSuperShape();
     }
 
 }
--- src/main/java/brickdestroy/elements/BrickFactory.java
+++ src/main/java/brickdestroy/elements/BrickFactory.java
@@ -3,12 +3,23 @@ package brickdestroy.elements;
 import java.awt.Dimension;
 import java.awt.Point;
 
+/**
+ * A factory for making bricks.
+ */
 public class BrickFactory {
 
     public static final int CLAY = 1;
     public static final int STEEL = 2;
     public static final int CEMENT = 3;
 
+    /**
+     * Makes a brick of the specified type. Idk what else to say
+     * 
+     * @param point The top-left point
+     * @param size  The size
+     * @param type  The type of brick
+     * @return
+     */
     public static Brick makeBrick(Point point, Dimension size, int type) {
         switch (type) {
             case CLAY:
