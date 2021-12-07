3c836f1778b2edac59bdb3ac8c44439681b6078e
Izzat Z <hcyiz1@nottingham.edu.my>
2021-12-07 16:33:16 +0800

    Added javadocs in Main

--- src/main/java/brickdestroy/Main.java
+++ src/main/java/brickdestroy/Main.java
@@ -1,13 +1,17 @@
 package brickdestroy;
 
-import java.awt.EventQueue;
+import javax.swing.SwingUtilities;
 
 import brickdestroy.gui.MainFrame;
 
+/**
+ * The Main class of the game, which contains the {@code main} method. It simply
+ * instantiates the {@link MainFrame}.
+ */
 public class Main {
 
     public static void main(String[] args) {
-        EventQueue.invokeLater(() -> new MainFrame());
+        SwingUtilities.invokeLater(() -> new MainFrame());
     }
 
 }
