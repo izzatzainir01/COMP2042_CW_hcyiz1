95b4cbaf4ea72bc10f8b4dc83af60064ea19ed3d
Izzat Z <hcyiz1@nottingham.edu.my>
2021-12-07 12:55:39 +0800

    Improved javadocs in AbstractGameEndView

--- src/main/java/brickdestroy/gui/view/AbstractGameEndView.java
+++ src/main/java/brickdestroy/gui/view/AbstractGameEndView.java
@@ -5,6 +5,17 @@ import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 
+/**
+ * An abstract child class of {@link MyAbstractView} that represents the View
+ * for when the game is stopped due to completing a round or the game, or losing
+ * the game. This class predefines a template that extends its parents' template
+ * that all Game End Views follow. Since all Game End Views have an Exit button,
+ * this class also predefines the text and the action command of the Second
+ * button.
+ * <p>
+ * The {@code AbstractGameEndView} class's content consists of a title and a
+ * display of the score.
+ */
 public abstract class AbstractGameEndView extends MyAbstractView {
 
     public static final String EXIT = "END_VIEW";
@@ -14,17 +25,19 @@ public abstract class AbstractGameEndView extends MyAbstractView {
     private int score = 0;
 
     /**
-     * The {@code GameEndView} class is a View class that represents the View for
-     * when the game is stopped due to completing a round/game, or losing the game.
+     * An abstract child class of {@link MyAbstractView} that represents the View
+     * for when the game is stopped due to completing a round or the game, or losing
+     * the game. This class predefines a template that extends its parents' template
+     * that all Game End Views follow. Since all Game End Views have an Exit button,
+     * this class also predefines the text and the action command of the Second
+     * button.
      * <p>
-     * It is responsible for defining the common components that make up the
-     * different variations of this View and adding them to itself. It contains a
-     * 'Secondary' button that users can define its text and action from the
-     * controller.
+     * The {@code AbstractGameEndView} class's content consists of a title and a
+     * display of the score.
      * 
-     * @param title      The title of the view
-     * @param score      The score that is displayed on the view
-     * @param button1Text The text on the Secondary button
+     * @param title          The title of this view
+     * @param button1Text    The text of the First button
+     * @param button1Command The action command of the First button
      */
     public AbstractGameEndView(String title, String button1Text, String button1Command) {
         // Call the super constructor
@@ -35,7 +48,7 @@ public abstract class AbstractGameEndView extends MyAbstractView {
     }
 
     /**
-     * Draw the background image and texts.
+     * Draw the background image, the title and the score.
      */
     @Override
     public void paintComponent(Graphics g) {
@@ -64,7 +77,7 @@ public abstract class AbstractGameEndView extends MyAbstractView {
     }
 
     /**
-     * Set the score of this view.
+     * Sets the score of this view.
      * 
      * @param score The new score
      */
